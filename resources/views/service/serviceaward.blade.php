c7a0b8ae5b5dc3413e09012a8527f4615c9987c3
Win <win@Wins-MacBook-Pro.local>
2025-02-19 12:07:09 -0800

    All pages finished except transport, start implementing language localization

--- resources/views/service/serviceaward.blade.php
+++ resources/views/service/serviceaward.blade.php
@@ -44,7 +44,7 @@
     <!-- Awards -->
     <div class="bg-white rounded-lg shadow-lg p-6">
         <h6 class="text-xl font-semibold text-gray-800 mb-4">
-        2020
+        2020 Awards
         </h6>
             <!-- Spare Section -->
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
@@ -54,12 +54,59 @@
                             <!-- Image Section -->
                             <div class="relative aspect-[4/3] w-full">
                                     <img 
-                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/3_years_award_1.jpg" 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/excellent_award.jpg" 
                                         class="w-full h-full object-cover rounded-t-lg"
                                     >
                             </div>
                             <div class="p-4">
-                                <p class="text-sm text-gray-600 mt-2">MMK</p>
+                                <p class="text-sm text-gray-600 mt-2">Excellent Award</p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/after_sale_service-award_1st_prize.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">After Sale Service-Award 1st Prize
+
+</p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/spare_2nd_prize.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">Spare 2nd prize</p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/fy_2020.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">FY 2020</p>
                             </div>
                         </div>
                     </a>
@@ -69,22 +116,36 @@
     <!-- Awards -->
     <div class="bg-white rounded-lg shadow-lg p-6">
         <h6 class="text-xl font-semibold text-gray-800 mb-4">
-        2020
+        2021 Awards
         </h6>
             <!-- Spare Section -->
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+            <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/after_sale_service-gold_prize.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">After Sale Service-Gold Prize</p>
+                            </div>
+                        </div>
+                    </a>
                     <a  class="block">
                         <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
                             <!-- Image Section -->
                             <div class="relative aspect-[4/3] w-full">
                                     <img 
-                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/3_years_award_1.jpg" 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/spare_parts-gold_prize.jpg" 
                                         class="w-full h-full object-cover rounded-t-lg"
                                     >
                             </div>
                             <div class="p-4">
-                                <p class="text-sm text-gray-600 mt-2">MMK</p>
+                                <p class="text-sm text-gray-600 mt-2">Spare Parts-Gold Prize</p>
                             </div>
                         </div>
                     </a>
@@ -94,22 +155,50 @@
         <!-- Awards -->
     <div class="bg-white rounded-lg shadow-lg p-6">
         <h6 class="text-xl font-semibold text-gray-800 mb-4">
-        2020
+        2022 Awards
         </h6>
             <!-- Spare Section -->
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
                     <a  class="block">
                         <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
                             <!-- Image Section -->
                             <div class="relative aspect-[4/3] w-full">
                                     <img 
-                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/3_years_award_1.jpg" 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/excellent_award_2022.jpg" 
                                         class="w-full h-full object-cover rounded-t-lg"
                                     >
                             </div>
                             <div class="p-4">
-                                <p class="text-sm text-gray-600 mt-2">MMK</p>
+                                <p class="text-sm text-gray-600 mt-2">Excellent Award</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/after_sale_service-award_1st_prize_2022.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">After Sale Service-1st Prize</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/spare_parts-winner.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">Spare Parts-Winner</p>
                             </div>
                         </div>
                     </a>
@@ -117,7 +206,7 @@
             </div>
         </div>
 
-        
+
 </div>   
  
         
--- resources/views/service/transport.blade.php
+++ resources/views/service/transport.blade.php
@@ -1,3 +1,184 @@
 <x-app-layout>
+    <!-- Header Section -->
+    <div class="bg-white">
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <h3 class="text-2xl font-semibold text-green-600">
+            ပြင်ပကားအငှား ဝန်ဆောင်မှု
+            </h3>
+            <p class="text-gray-600 mt-4 mb-4 leading-8">
+            ရိတ်သိမ်းခြွေလှ့စက်၊ လေးဘီးထွန်စက်များ သယ်ယူပို့ဆောင်ရာတွင် တောင်သူများ အဆင်ပြေစေရန်အတွက် အကျိုးအမြတ်မယူပဲ သတင်းအချက်အလက် မျှဝေထားပေးခြင်းသာဖြစ်ပါသည်။</p>
+        </div>
+    </div>
+
     
+
+    <!-- Awards -->
+    <div class="bg-white rounded-lg shadow-lg p-6">
+        <h6 class="text-xl font-semibold text-gray-800 mb-4">
+        2020 Awards
+        </h6>
+            <!-- Spare Section -->
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/excellent_award.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">Excellent Award</p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/after_sale_service-award_1st_prize.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">After Sale Service-Award 1st Prize
+
+</p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/spare_2nd_prize.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">Spare 2nd prize</p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/fy_2020.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">FY 2020</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    <!-- Awards -->
+    <div class="bg-white rounded-lg shadow-lg p-6">
+        <h6 class="text-xl font-semibold text-gray-800 mb-4">
+        2021 Awards
+        </h6>
+            <!-- Spare Section -->
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+            <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/after_sale_service-gold_prize.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">After Sale Service-Gold Prize</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/spare_parts-gold_prize.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">Spare Parts-Gold Prize</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- Awards -->
+    <div class="bg-white rounded-lg shadow-lg p-6">
+        <h6 class="text-xl font-semibold text-gray-800 mb-4">
+        2022 Awards
+        </h6>
+            <!-- Spare Section -->
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/excellent_award_2022.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">Excellent Award</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/after_sale_service-award_1st_prize_2022.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">After Sale Service-1st Prize</p>
+                            </div>
+                        </div>
+                    </a>
+                    <a  class="block">
+                        <div class="bg-white rounded-lg shadow hover:shadow-lg transition-shadow duration-200">
+                            <!-- Image Section -->
+                            <div class="relative aspect-[4/3] w-full">
+                                    <img 
+                                        src="https://sys-shop.s3.ap-southeast-1.amazonaws.com/0main/vendor/image/award/spare_parts-winner.jpg" 
+                                        class="w-full h-full object-cover rounded-t-lg"
+                                    >
+                            </div>
+                            <div class="p-4">
+                                <p class="text-sm text-gray-600 mt-2">Spare Parts-Winner</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+</div>   
+ 
+        
 </x-app-layout>
