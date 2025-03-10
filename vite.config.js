5083e04a064bde1ffbe04ecaee67eaff63a13a10
Win <win@Wins-MacBook-Pro.local>
2025-03-09 17:24:45 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/carousel.css', 'resources/js/app.js', 'resources/js/carousel.js', 'resources/css/bootstrap.division.css'],
+            input: ['resources/css/app.css', 'resources/css/carousel.css', 'resources/js/app.js', 'resources/js/carousel.js', 'resources/css/bootstrap.division.css', 'resources/css/semantic.division.css','resources/css/app.division.css','resources/css/index.division.css'],
             refresh: true,
         }),
     ],
