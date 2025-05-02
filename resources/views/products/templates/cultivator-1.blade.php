95bbd50f5e80698425aa848fc8b666edb88185b2
kaungsanthwin <kaungsanthwin388@gmail.com>
2025-05-02 16:37:17 +0630

    Initial commit with Laravel setup and configuration

---
+++ resources/views/products/templates/cultivator-1.blade.php
@@ -0,0 +1,32 @@
+{{-- resources/views/products/gt-1.blade.php --}}
+<div class="specification-container">
+    <table class="table-striped">
+        <tbody>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Horse Power</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">16 HP (Engine Model: S 1100 N)</td>
+            </tr>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Weight</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">480 kg</td>
+            </tr>
+            <!-- <tr class="text-grey-color">
+                <td class="py-1 fs-6">Wheel</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">900 x 1920 (mm)</td>
+            </tr>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Width</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">2285 (mm)</td>
+            </tr>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Height</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">2280 (mm)</td>
+            </tr> -->
+        </tbody>
+    </table>
+</div>
---
+++ resources/views/products/templates/polyplow-2.blade.php
@@ -0,0 +1,32 @@
+{{-- resources/views/products/gt-1.blade.php --}}
+<div class="specification-container">
+    <table class="table-striped">
+        <tbody>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Horse Power</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6"> 70–90 HP</td>
+            </tr>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Weight</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">598 kg</td>
+            </tr>
+            <!-- <tr class="text-grey-color">
+                <td class="py-1 fs-6">Wheel</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">900 x 1920 (mm)</td>
+            </tr>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Width</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">2285 (mm)</td>
+            </tr>
+            <tr class="text-grey-color">
+                <td class="py-1 fs-6">Height</td>
+                <td class="ps-4 pe-2">-</td>
+                <td class="text-dark fs-6">2280 (mm)</td>
+            </tr> -->
+        </tbody>
+    </table>
+</div>
