8b0d54b82f50a0ae079c3f7ad70eb164bc9e3859
kyisinthein <kyisinthein6940@gmail.com>
2025-06-30 17:52:54 +0630

    Added some changes

--- resources/views/products/templates/tf120_12hp.blade.php
+++ resources/views/products/templates/tf120_12hp.blade.php
@@ -54,7 +54,284 @@
 </div>
 </div>
 
+<div class="max-w-7xl mx-auto p-6 lg:p-8 space-y-8">
+    <div class="bg-white rounded-lg shadow-lg p-6">
+        <br>
+        <h6 class="text-xl font-semibold text-gray-800 mb-4">
+        အဓိကအင်္ဂါရပ်များ
+        </h6>
+            <!-- Spare Section -->
+        <div class="max-w-7xl mx-auto py-6 lg:py-8">
+            <div class="space-y-4">
+                <!-- Dropdown 1 -->
+                <div class="border border-gray-200 rounded-lg overflow-hidden">
+                    <button 
+                        onclick="toggleMainFeature('feature1')" 
+                        class="w-full flex items-center justify-between px-6 py-4 bg-white hover:bg-gray-50 focus:outline-none transition-colors duration-150"
+                    >
+                        <div class="flex items-center">
+                            <span class="text-lg font-medium text-gray-700 text-left" style="font-size: 18px;">စွမ်းဆောင်ရည်မြင့်မားသော TF120 Yanmar အင်ဂျင်</span>
+                        </div>
+                        <svg 
+                            id="arrowFeature1" 
+                            class="w-5 h-5 text-gray-500 transform transition-transform duration-200 flex-shrink-0 ml-4" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            viewBox="0 0 24 24" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div id="feature1" class="hidden border-t border-gray-200 bg-gray-50">
+                        <div class="md:flex md:flex-row-reverse md:items-start">
+                            <div class="relative aspect-[4/3] w-full md:w-1/3 md:p-4">
+                                <img
+                                    src="https://www.yanmar.com/media/news/2020/01/17045933/img_gt-1_02.jpg"
+                                    class="w-full h-full object-cover md:rounded-lg"
+                                >
+                            </div>
+                            <div class="p-4 md:w-2/3 md:pl-8">
+                                <p class="text-sm text-gray-600 mt-2 leading-8">
+                                GT-! လက်တွန်းထွန်စက်သည် ဒီဇိုင်းအသစ်Yanmar TF120 အင်ဂျင်ဖြင့် တွဲဖက်အသုံးပြုထားပြီး အမြင့်ဆုံးမြင်းကောင်ရေ ၁၅ ကောင်ထိ တွဲဖက်အသုံးပြုနိုင်ပါသည်။ထို့အပြင် သက်တောင့်သက်သာ မောင်းနှင်နိုင်ခြင်း၊ စွမ်းအားပြည့်ထုတ်ပေးနိုင်ခြင်း တို့ကြောင့် ဆီစားနှုန်းသက်သက်သာစွာဖြင့် မောင်းနှင်နှိုင်မှာဖြစ်ပါသည်။
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <!-- Dropdown 2 -->
+                <div class="border border-gray-200 rounded-lg overflow-hidden">
+                    <button 
+                        onclick="toggleMainFeature('feature2')" 
+                        class="w-full flex items-center justify-between px-6 py-4 bg-white hover:bg-gray-50 focus:outline-none transition-colors duration-150"
+                    >
+                        <div class="flex items-center">
+                            <span class="text-lg font-medium text-gray-700 text-left">တိုက်ရိုက်ဆီဖြန်းစနစ်                            </span>
+                        </div>
+                        <svg 
+                            id="arrowFeature2" 
+                            class="w-5 h-5 text-gray-500 transform transition-transform duration-200 flex-shrink-0 ml-4" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            viewBox="0 0 24 24" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div id="feature2" class="hidden border-t border-gray-200 bg-gray-50">
+                        <div class="md:flex md:flex-row-reverse md:items-start">
+                            <div class="relative aspect-[4/3] w-full md:w-1/3 md:p-4">
+                                <img
+                                    src="https://www.yanmar.com/media/news/2020/01/17050303/img_gt-1_03.jpg"
+                                    class="w-full h-full object-cover md:rounded-lg"
+                                >
+                            </div>
+                            <div class="p-4 md:w-2/3 md:pl-8">
+                                <p class="text-sm text-gray-600 mt-2 leading-8">
+                                Yanmar ၏ တိုက်ရိုက်ဆီဖြန်းသည့်စနှစ်သည် စက်၏ဆီစားမှုနှုန်းကို အထူလျှော့ချပေးသည့်အတွက် ဆီစားနှုန်းသက်သာစွာ ဖြင့်မောင်းနှင်နှိုင်မှာဖြစ် ပါသည်။
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Dropdown 3 -->
+                <div class="border border-gray-200 rounded-lg overflow-hidden">
+                    <button 
+                        onclick="toggleMainFeature('feature3')" 
+                        class="w-full flex items-center justify-between px-6 py-4 bg-white hover:bg-gray-50 focus:outline-none transition-colors duration-150"
+                    >
+                        <div class="flex items-center">
+                            <span class="text-lg font-medium text-gray-700 text-left">တုန်ခါမှု လျှေ့ာချပေးနိုင်ခြင်း                            </span>
+                        </div>
+                        <svg 
+                            id="arrowFeature3" 
+                            class="w-5 h-5 text-gray-500 transform transition-transform duration-200 flex-shrink-0 ml-4" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            viewBox="0 0 24 24" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div id="feature3" class="hidden border-t border-gray-200 bg-gray-50">
+                        <div class="md:flex md:flex-row-reverse md:items-start">
+                            <div class="relative aspect-[4/3] w-full md:w-1/3 md:p-4">
+                                <img
+                                    src="https://www.yanmar.com/media/news/2020/01/17050944/img_gt-1_04.jpg"
+                                    class="w-full h-full object-cover md:rounded-lg"
+                                >
+                            </div>
+                            <div class="p-4 md:w-2/3 md:pl-8">
+                                <p class="text-sm text-gray-600 mt-2 leading-8">
+                                Yanmar ၏ ပြိုင်ဘက်ကင်းသော ဟန်ချက်ထိန်းတန်းနှစ်ခုသည် စက်၏ အရှိန် နှင့် အသံများကို လျော့ချခြင်းဖြင့် အသံကျယ်လောင်မှု ကို ဖယ်ရှားပေးနိုင်ပါသည်။ ဟန်ချက်မညီမျှမှုများကိုလည်း အတွင်းပိုင်းတွင် ထိန်းညှိပေးထားသောကြောင့် စက်တုန်ခါမှုကို လျော့နည်းစေပါသည်။ ထိုအပြင် အရွယ်အစားကြီးမားသော အသံထိန်း အိတ်ဇောပါသောကြောင့် အသံ ဆူညံခြင်းအား လျေ့ာကျစေပါသည်။
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Dropdown 4 -->
+                <div class="border border-gray-200 rounded-lg overflow-hidden">
+                    <button 
+                        onclick="toggleMainFeature('feature4')" 
+                        class="w-full flex items-center justify-between px-6 py-4 bg-white hover:bg-gray-50 focus:outline-none transition-colors duration-150"
+                    >
+                        <div class="flex items-center">
+                            <span class="text-lg font-medium text-gray-700 text-left">အလွယ်တကူမြင်နိုင်သော ဆီရေစစ်                            </span>
+                        </div>
+                        <svg 
+                            id="arrowFeature4" 
+                            class="w-5 h-5 text-gray-500 transform transition-transform duration-200 flex-shrink-0 ml-4" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            viewBox="0 0 24 24" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div id="feature4" class="hidden border-t border-gray-200 bg-gray-50">
+                        <div class="md:flex md:flex-row-reverse md:items-start">
+                            <div class="relative aspect-[4/3] w-full md:w-1/3 md:p-4">
+                                <img
+                                    src="https://www.yanmar.com/media/news/2020/01/17051254/img_gt-1_05.jpg"
+                                    class="w-full h-full object-cover md:rounded-lg"
+                                >
+                            </div>
+                            <div class="p-4 md:w-2/3 md:pl-8">
+                                <p class="text-sm text-gray-600 mt-2 leading-8">
+                                ဆီစစ်အောက်ခြေတွင် ရေပါ၀င်လာမှု ပမာဏကို လွယ်ကူစွာ စစ်ဆေးနိုင်ပါသည်။ ဆီစစ်တွင် အလိုလျောက် လေရှူစနစ် ပါရှိ ‌‌သောကြောင့် မည်သည့် ကိရိယာမှ အသုံးမပြုပဲ လွယ်ကူစွာ လုပ်ဆောင်နိုင်ပါသည်။
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Dropdown 5 -->
+                <div class="border border-gray-200 rounded-lg overflow-hidden">
+                    <button 
+                        onclick="toggleMainFeature('feature5')" 
+                        class="w-full flex items-center justify-between px-6 py-4 bg-white hover:bg-gray-50 focus:outline-none transition-colors duration-150"
+                    >
+                        <div class="flex items-center">
+                            <span class="text-lg font-medium text-gray-700 text-left">အင်ဂျင်ဝိုင် အချက်ပြစနစ်                            </span>
+                        </div>
+                        <svg 
+                            id="arrowFeature5" 
+                            class="w-5 h-5 text-gray-500 transform transition-transform duration-200 flex-shrink-0 ml-4" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            viewBox="0 0 24 24" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div id="feature5" class="hidden border-t border-gray-200 bg-gray-50">
+                        <div class="md:flex md:flex-row-reverse md:items-start">
+                            <div class="relative aspect-[4/3] w-full md:w-1/3 md:p-4">
+                                <img
+                                    src="https://www.yanmar.com/media/news/2020/01/17051530/img_gt-1_06.jpg"
+                                    class="w-full h-full object-cover md:rounded-lg"
+                                >
+                            </div>
+                            <div class="p-4 md:w-2/3 md:pl-8">
+                                <p class="text-sm text-gray-600 mt-2 leading-8">
+                                အင်ဂျင်ဝိုင်၏ ဖိအား အတိုင်းတာအား အရောင်ကို ကြည့်ခြင်းအားဖြင့် လွယ်ကူစွာ စစ်ဆေးနိုင်ပါသည်။ အပြာရောင် – ပုံမှန်အခြေအနေ / အနီရောင်- ပုံမှန် မဟုတ်သော အခြေအနေ
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Dropdown 6 -->
+                <div class="border border-gray-200 rounded-lg overflow-hidden">
+                    <button 
+                        onclick="toggleMainFeature('feature6')" 
+                        class="w-full flex items-center justify-between px-6 py-4 bg-white hover:bg-gray-50 focus:outline-none transition-colors duration-150"
+                    >
+                        <div class="flex items-center">
+                            <span class="text-lg font-medium text-gray-700 text-left">လွယ်ကူစွာ ကြိုးချိန်ညှိနိုင်ခြင်း                            </span>
+                        </div>
+                        <svg 
+                            id="arrowFeature6" 
+                            class="w-5 h-5 text-gray-500 transform transition-transform duration-200 flex-shrink-0 ml-4" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            viewBox="0 0 24 24" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div id="feature6" class="hidden border-t border-gray-200 bg-gray-50">
+                        <div class="md:flex md:flex-row-reverse md:items-start">
+                            <div class="relative aspect-[4/3] w-full md:w-1/3 md:p-4">
+                                <img
+                                    src="https://www.yanmar.com/media/news/2020/01/17051753/img_gt-1_07.jpg"
+                                    class="w-full h-full object-cover md:rounded-lg"
+                                >
+                            </div>
+                            <div class="p-4 md:w-2/3 md:pl-8">
+                                <p class="text-sm text-gray-600 mt-2 leading-8">
+                                မည်သည့်ကိရိယာမှ အသုံးမလိုပဲ ပူလီကြိုးကို လွယ်ကူစွာ ချိန်ညှိနိုင်ပါသည်။
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Dropdown 7 -->
+                <div class="border border-gray-200 rounded-lg overflow-hidden">
+                    <button 
+                        onclick="toggleMainFeature('feature7')" 
+                        class="w-full flex items-center justify-between px-6 py-4 bg-white hover:bg-gray-50 focus:outline-none transition-colors duration-150"
+                    >
+                        <div class="flex items-center">
+                            <span class="text-lg font-medium text-gray-700 text-left">လေစစ် ( အစိုစစ်)                            </span>
+                        </div>
+                        <svg 
+                            id="arrowFeature7" 
+                            class="w-5 h-5 text-gray-500 transform transition-transform duration-200 flex-shrink-0 ml-4" 
+                            fill="none" 
+                            stroke="currentColor" 
+                            viewBox="0 0 24 24" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div id="feature7" class="hidden border-t border-gray-200 bg-gray-50">
+                        <div class="md:flex md:flex-row-reverse md:items-start">
+                            <div class="relative aspect-[4/3] w-full md:w-1/3 md:p-4">
+                                <img
+                                    src="https://www.yanmar.com/media/news/2020/01/17052051/img_gt-1_08.jpg"
+                                    class="w-full h-full object-cover md:rounded-lg"
+                                >
+                            </div>
+                            <div class="p-4 md:w-2/3 md:pl-8">
+                                <p class="text-sm text-gray-600 mt-2 leading-8">
+                                မည်မျှသေးငယ်သော ဖုန်မှုန့်များကိုမဆို လွတ်ထွတ်မသွားအောင် ဆီသုံး၍ စစ်ထုတ်နိုင်သောကြောင့် သန့်စင်သောလေကို အင်ဂျင်အတွက် ပေးစွမ်းနိုင်ပါသည်။
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+
+   
+
+    <!-- SECOND SECTION -->
+   
+    </div>
 
 
     <!-- <div class="max-w-7xl mx-auto p-6 lg:p-8 space-y-8">
